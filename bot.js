3e31e1a151659ec930492ff9acc4c116f3aa97bb
mahbodsr <srmahbod@gmail.com>
2024-04-21 12:10:06 +0330

    initial commit

---
+++ bot.js
@@ -0,0 +1,23 @@
+import { Bot } from "grammy";
+
+const bot = new Bot("6666871515:AAHkpjn_aSj08IItJzELf4xr-7qarSMcnw0"); // <-- Enter Bot Token Here
+
+bot.hears("ثانیه", (ctx) => {
+  if (!ctx.message?.reply_to_message) return;
+  const msg = ctx.message.reply_to_message;
+  const fullName = [msg.from?.first_name, msg.from?.last_name].join(" ");
+  const date = new Date(ctx.message.reply_to_message.date * 1000);
+  const t = date.toLocaleTimeString("fa-IR-u-nu-latn", {
+    hour12: false,
+  });
+  const d = date.toLocaleDateString("fa-IR-u-nu-latn");
+  ctx.reply(
+    `┎ <a href="tg://user?id=${msg.from.id}">${fullName}</a>\n┣ ${d}\n┖ ${t}`,
+    {
+      reply_parameters: { message_id: ctx.message.reply_to_message.message_id },
+      parse_mode: "HTML",
+    }
+  );
+});
+
+bot.start({ onStart: () => console.log("Bot Started Successfuly") });
